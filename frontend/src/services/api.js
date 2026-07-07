8517a85092b9f60e744115a5baf623844c75aa04
Gaurav Singh Chauhan <gkumarc121@gmail.com>
2026-07-07 14:28:23 +0530

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://project-task-management-system-vjl6.vercel.app/",
+  baseURL: "https://project-task-management-system-vjl6.vercel.app/api",
 });
 
 api.interceptors.request.use((config) => {
